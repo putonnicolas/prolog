2dcf3a4917e99b1887729bd2d2a412bf51129e28
lorisveron <loris.veron@insa-lyon.fr>
2025-12-02 09:49:53 +0100

    Premieres win conditions

--- main.pl
+++ main.pl
@@ -41,3 +41,35 @@ affiche_lignes(Board, Ligne) :-
     affiche_ligne(Board, Ligne),
     Ligne1 is Ligne - 1,
     affiche_lignes(Board, Ligne1).
+
+win(Board) :- 
+    win_ligne(Board); 
+    win_colonne(Board); 
+    win_diago(Board). 
+
+alignees(P, P, P, P) :-
+    P \= vide.
+
+win_ligne(Board) :-
+    between(1, 6, Ligne),
+    between(1, 4, Col),
+    Col1 is Col + 1,
+    Col2 is Col + 2,
+    Col3 is Col + 3,
+    piece_a(Board, Ligne, Col,  P1),
+    piece_a(Board, Ligne, Col1, P2),
+    piece_a(Board, Ligne, Col2, P3),
+    piece_a(Board, Ligne, Col3, P4),
+    alignees(P1, P2, P3, P4).
+
+win_colonne(Board) :-
+    between(1, 3, Ligne),
+    between(1, 7, Col),
+    Ligne1 is Ligne + 1,
+    Ligne2 is Col + 2,
+    Ligne3 is Col + 3,
+    piece_a(Board, Ligne, Col,  P1),
+    piece_a(Board, Ligne2, Col, P2),
+    piece_a(Board, Ligne3, Col, P3),
+    piece_a(Board, Ligne4, Col, P4),
+    alignees(P1, P2, P3, P4).
