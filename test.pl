c13bdd40c2ae75e0de1a149c71d89aea1567d569
SphAthn <sophie.anthoine@gmail.com>
2025-12-09 11:38:08 +0100

    New tests

--- test.pl
+++ test.pl
@@ -8,6 +8,58 @@ test(plateau_vide) :-
     plateau_initial(Board),
     Board == [[], [], [], [], [], [], []].
 
+test(win_colonne) :-
+    Board = [
+        [x, x, x, x],
+        [o, o, o],
+        [],
+        [],
+        [],
+        [],
+        []
+    ],
+    win(Board, 4, 1).
+
+test(win_ligne) :-
+    Board = [  
+        [o, x],
+        [o, x],
+        [o],
+        [o],
+        [],
+        [],
+        [x]
+    ],
+    win(Board, 1, 3).
+
+test(win_diagonale) :-
+    % Diagonale sens 1 (haut-gauche vers bas-droite):
+    % piece_a(Board, 1, 1, x)
+    % piece_a(Board, 2, 2, x)
+    % piece_a(Board, 3, 3, x)
+    % piece_a(Board, 4, 4, x)
+    Board = [
+        [x],               % Colonne 1: [x] -> position (1,1)
+        [o, x],            % Colonne 2: [o, x] -> positions (1,2), (2,2)
+        [o, o, x],         % Colonne 3: [o, o, x] -> positions (1,3), (2,3), (3,3)
+        [o, o, o, x],      % Colonne 4: [o, o, o, x] -> positions (1,4), (2,4), (3,4), (4,4)
+        [],
+        [],
+        []
+    ],
+    win(Board, 4, 4).
+
+test(board_full) :-
+    Board = [
+        [o, o, x, o, x, o],
+        [x, x, x, o, x, x],
+        [x, o, o, x, x, o],
+        [o, x, x, x, o, x],
+        [x, o, o, o, x, o],
+        [o, x, x, o, x, o],
+        [x, x, x, o, x, o]
+    ],
+    plateau_plein(Board).
 
 
 :- end_tests(puissance4_tests).
